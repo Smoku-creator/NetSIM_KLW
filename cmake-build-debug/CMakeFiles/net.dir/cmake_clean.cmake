5c34d4b6f7585631526c0fc3debcf9dc79c30f98
Wiktor Kacprzak <ceb.zabal5@gmail.com>
2021-12-19 18:51:38 +0100

    'Poprawa'

---
+++ cmake-build-debug/CMakeFiles/net.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/net.dir/main.cpp.obj"
+  "CMakeFiles/net.dir/src/package.cpp.obj"
+  "CMakeFiles/net.dir/src/storage_types.cpp.obj"
+  "libnet.dll.a"
+  "net.exe"
+  "net.exe.manifest"
+  "net.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/net.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
