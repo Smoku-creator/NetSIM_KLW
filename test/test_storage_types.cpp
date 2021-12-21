1e39b41682473bae0489f73934109f464aff2800
Wiktor Kacprzak <ceb.zabal5@gmail.com>
2021-12-21 21:23:01 +0100

    'Teściki_do_pobrania'

---
+++ test/test_storage_types.cpp
@@ -0,0 +1,38 @@
+//#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "package.hpp"
+#include "storage_types.hpp"
+#include "types.hpp"
+
+using ::std::cout;
+using ::std::endl;
+
+TEST(PackageQueueTest, IsFifoCorrect) {
+    PackageQueue q(PackageQueueType::FIFO);
+    Package k(1);
+    q.push(std::move(k));
+
+    Package r(2);
+    q.push(std::move(r));
+//    q.push(Package(1));
+//    q.push(Package(2));
+
+    Package p(std::move(q.pop()));
+    EXPECT_EQ(p.get_id(), 1);
+
+    p = q.pop();
+    EXPECT_EQ(p.get_id(), 2);
+}
+
+TEST(PackageQueueTest, IsLifoCorrect) {
+    PackageQueue q(PackageQueueType::LIFO);
+    q.push(Package(1));
+    q.push(Package(2));
+
+    Package p(std::move(q.pop()));
+    EXPECT_EQ(p.get_id(), 2);
+
+    p = q.pop();
+    EXPECT_EQ(p.get_id(), 1);
+}
