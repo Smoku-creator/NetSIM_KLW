12637d722a8920fbe5734bcca91e73419ca91fe1
Jakub Wasik <jwasik@student.agh.edu.pl>
2022-01-09 22:50:05 +0100

    'factory'

--- include/factory.hpp
+++ include/factory.hpp
@@ -9,27 +9,29 @@
 
 class Factory {
 public:
-    add_ramp(Ramp&&) : void
-    remove_ramp(id: ElementID) : void
-    find_ramp_by_id(id: ElementID) : NodeCollection<Ramp>::iterator
-    find_ramp_by_id(id: ElementID) : NodeCollection<Ramp>::const_iterator {query}
-    ramp_cbegin() : NodeCollection<Ramp>::const_iterator {query}
-    ramp_cend() : NodeCollection<Ramp>::const_iterator {query}
-    remove_receiver(collection: NodeCollection<Node>&, id: ElementID) : void
+    virtual void add_ramp(Ramp&&) = 0;
+    virtual void remove_ramp(id: ElementID) = 0;
+    NodeCollection<Ramp>::iterator find_ramp_by_id(id: ElementID);
+    NodeCollection<Ramp>::const_iterator find_ramp_by_id(id: ElementID);
+    NodeCollection<Ramp>::const_iterator ramp_cbegin();
+    NodeCollection<Ramp>::const_iterator ramp_cend();
+    void remove_receiver(collection: NodeCollection<Node>&, id: ElementID) : void;
 
-    add_worker(worker&&) : void
-    remove_worker(id: ElementID) : void
-    find_worker_by_id(id: ElementID) : NodeCollection<worker>::iterator
-    find_worker_by_id(id: ElementID) : NodeCollection<worker>::const_iterator {query}
-    worker_cbegin() : NodeCollection<worker>::const_iterator {query}
-    worker_cend() : NodeCollection<worker>::const_iterator {query
+    virtual void add_worker(worker&&) = 0;
+    virtual void remove_worker(id: ElementID) = 0;
+    NodeCollection<worker>::iterator find_worker_by_id(id: ElementID);
+    NodeCollection<worker>::const_iterator find_worker_by_id(id: ElementID);
+    NodeCollection<worker>::const_iterator worker_cbegin();
+    NodeCollection<worker>::const_iterator worker_cend();
+    void remove_receiver(collection: NodeCollection<Node>&, id: ElementID);
 
-    add_storehouse(storehouse&&) : void
-    remove_storehouse(id: ElementID) : void
-    find_storehouse_by_id(id: ElementID) : NodeCollection<storehouse>::iterator
-    find_storehouse_by_id(id: ElementID) : NodeCollection<storehouse>::const_iterator {query}
-    storehouse_cbegin() : NodeCollection<storehouse>::const_iterator {query}
-    storehouse_cend() : NodeCollection<storehouse>::const_iterator {query
+    virtual void add_storehouses(storehouses&&) = 0;
+    virtual void remove_storehouses(id: ElementID) = 0;
+    NodeCollection<storehouses>::iterator find_storehouses_by_id(id: ElementID);
+    NodeCollection<storehouses>::const_iterator find_storehouses_by_id(id: ElementID);
+    NodeCollection<storehouses>::const_iterator storehouses_cbegin();
+    NodeCollection<storehouses>::const_iterator storehouses_cend();
+    void remove_receiver(collection: NodeCollection<Node>&, id: ElementID);
 
     is_consistent(void) : bool {query}
 
@@ -42,8 +44,8 @@ public:
 
 class NodeCollection<Node> {
 public:
-    add(node: Node&&) : void
-    remove_by_id(id: ElementID) : void
-    find_by_id(id: ElementID) : NodeCollection<Node>::iterator
-    find_by_id(id: ElementID) : NodeCollection<Node>::const_iterator {query}
+    virtual void add(node: Node&&) = 0;
+    virtual void remove_by_id(id: ElementID) = 0;
+    NodeCollection<Node>::iterator find_by_id(id: ElementID);
+    NodeCollection<Node>::const_iterator find_by_id(id: ElementID);
 };
